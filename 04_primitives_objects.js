a464c9626e97e7528510d137b6c7f53c93236fad
Abhijit Das <abhijitd721201@gmail.com>
2023-10-10 17:55:47 +0530

    datatypes and objects

---
+++ 04_primitives_objects.js
@@ -0,0 +1,37 @@
+// Premitives datatyep:-> 7 types of datatypes in javascript 
+let a=null
+let b=200
+let c=Symbol("I am a nice symbol")
+let d="Abhijit"
+let e=true
+let f=BigInt("567")
+let g=undefined
+
+console.log(a)
+
+console.log(b)
+
+console.log(c)
+
+console.log(d)
+
+console.log(e)
+
+console.log(f)
+
+console.log(g)
+
+console.log(typeof f)
+
+
+// Objects are non-premitives datatypes..
+// Objects in js(key and value pair call objects in js)
+
+const items={
+    "name" : "Abhijit",
+    "roll" : 27,
+    "address": undefined,
+    "age" : 23
+
+}
+console.log(items['name'])
