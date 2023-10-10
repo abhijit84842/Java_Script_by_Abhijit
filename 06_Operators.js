56af359ee6e4919e31701412e6c30b5ae0d0cef4
Abhijit Das <abhijitd721201@gmail.com>
2023-10-10 18:50:45 +0530

    some type of operators

---
+++ 06_Operators.js
@@ -0,0 +1,61 @@
+// Operator in js 
+
+
+// Arithmetic operators..
+let a=20
+let b=5
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a**b)
+console.log(a%b)
+console.log(a/b)
+console.log(++a)        // output -> 21
+console.log(--a)         // output -> 20
+
+
+// Assingment operatos..
+
+let x=2
+x+=5
+console.log(x)
+
+x**=5
+console.log(x)
+
+
+
+//comparision operators
+let comp1=5;
+let comp2=8;
+let comp3="8"       //string 
+console.log(comp1==comp2)       // false
+
+console.log(comp1!=comp2)       // true
+
+console.log(comp1===comp2)      // false
+
+console.log(comp1!==comp2)      //true
+
+console.log(comp2==comp3)       // true (bacause value is same)
+
+console.log(comp2!=comp3)       //false
+
+console.log(comp2===comp3)      //false(it will show the orginal value)
+
+console.log(comp2!==comp3)      // true
+
+
+// logical operators..
+
+let x1=5;
+let y1=6;
+
+console.log(x1>y1 && x1==5)     // false
+
+console.log(x1<y1 && y1==6)     // true
+
+console.log(x1>y1 || x1==5)     //true
+
+console.log(x1>y1 || y1==8)     //false
