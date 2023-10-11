2d281474dd983130cab4457f2d210bef6072b50d
Abhijit Das <abhijitd721201@gmail.com>
2023-10-11 11:54:10 +0530

    prompt run in vs code

--- 08_Practice_set_on_operators.js
+++ 08_Practice_set_on_operators.js
@@ -1,6 +1,6 @@
 // take a input of age from user so that age will be 10-20.. 
 
-
+const prompt =require("prompt-sync")({sigint: true});       // to run the prompt in vs code..
 let age =prompt("Enter your age : ")
 
 if (age>=10 && age<=20){
