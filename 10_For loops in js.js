aa66462e62f2635a6beb6ce824453ef8e2169f3e
Abhijit Das <abhijitd721201@gmail.com>
2023-10-11 12:33:43 +0530

    Types of for loop

---
+++ 10_For loops in js.js
@@ -0,0 +1,27 @@
+// for loop in js..
+for (let i=0; i<=35; i++){
+    console.log(i)
+}
+
+
+// sum of n natural number ...
+sum=0
+n=parseInt(prompt("Enter the n number : "))
+
+for (let i=0; i<n; i++){
+    sum +=(i+1)
+}
+console.log(sum)
+
+
+// for in loop..
+
+let obj={
+    'Abhi': 50,
+    'raj' : 60,
+    'rohan' : 80
+}
+
+for (let i in obj){
+    console.log(obj[i])
+}
