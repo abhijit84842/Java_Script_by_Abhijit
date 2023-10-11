b33a42ed7257e91f74af8a0b838b7f47c81e673f
Abhijit Das <abhijitd721201@gmail.com>
2023-10-11 11:25:18 +0530

    if else pratice

---
+++ 08_Practice_set_on_operators.js
@@ -0,0 +1,13 @@
+// atke a input of age from user so that age will be 10-20.. 
+
+
+let age =prompt("Enter your age : ")
+
+if (age>=10 && age<=20){
+    alert("you are eligible..")
+}
+else{
+    alert("not eligible..")
+}
+
+
