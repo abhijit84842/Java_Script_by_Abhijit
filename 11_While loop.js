362ceaa97841e6decf0a4cb65e868bad16cb6d7b
Abhijit Das <abhijitd721201@gmail.com>
2023-10-13 17:07:42 +0530

    explane while loop

---
+++ 11_While loop.js
@@ -0,0 +1,33 @@
+// while loop..
+/*
+let n =parseInt(prompt("enter the number: "))
+let i =0
+while(i<n){
+    console.log(i)
+    i=i+1
+}
+
+*/
+
+// do while loop
+//first it check block code then check the condition.
+
+
+let n1 =parseInt(prompt("enter the number: "))
+i=0
+do{
+    console.log(i)
+    i++
+}
+while(i<n1)
+
+
+// do whiile loop always print one time before checking the condition. 
+
+let n2 =parseInt(prompt("enter the number: "))
+i=10
+do{
+    console.log(i)      // it will print 10 after that condition will be false...
+    i++
+}
+while(i<n2)
