98b46686cb71a4d371901db31c833002b18b3685
Abhijit Das <abhijitd721201@gmail.com>
2023-10-13 17:40:17 +0530

    strings and escape sequence

---
+++ 13_Strings.js
@@ -0,0 +1,22 @@
+let s="abhijit"
+console.log(s)
+
+console.log(s.length)
+
+
+// index of string
+console.log(s[0])
+
+
+//Tamplate literals..
+let boy1="parmod"
+let boy2="Adi"
+
+let friend = `${boy1} is a friend of ${boy2}`
+console.log(friend)
+
+
+//Escape sequence..
+
+let city='chandrakona\'town'
+console.log(city)
