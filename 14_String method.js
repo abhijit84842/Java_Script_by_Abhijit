34cd707d0ca091972b1cbfe8a2d9d6d279f0f959
Abhijit Das <abhijitd721201@gmail.com>
2023-10-17 12:16:22 +0530

    type of string method

---
+++ 14_String method.js
@@ -0,0 +1,50 @@
+let name= "javascript"
+console.log(name.length)        // output => 10
+
+// escape sequense...
+
+let college="techno\""
+console.log(college.length)     // output => 7
+
+
+// convert in uppercase..
+console.log(name.toUpperCase())
+
+// convert to lower case..
+console.log(name.toLowerCase())
+
+
+
+// strings slicing..
+let name1="python"
+console.log(name1.slice(2,5))       //output => tho
+
+// replace method..
+console.log(name1.replace("p","b"))
+
+
+// trim method..
+let city="    chandrakona town     "
+console.log(city.length)      
+console.log(city.trim())        // it remove the spaces..
+
+
+
+
+// string are immutable (ones it created it can't change..)
+let a ="Coding"
+a[0]="D"
+console.log(a)  // cant not change, because string is immutable..
+
+
+
+
+
+
+
+
+
+
+
+
+
