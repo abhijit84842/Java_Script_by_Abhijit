c97a748f69500727cc840b1734e1407e8426f0b7
Abhijit Das <abhijitd721201@gmail.com>
2023-11-09 12:30:34 +0530

    #

---
+++ 26_alert_prompt & confirm.js
@@ -0,0 +1,17 @@
+
+
+// to show a alert 
+alert("hi you are successfully login")
+
+// a take input by prompt command
+alert("enter the value...")
+let a=parseInt(prompt("enter the value of a: "))
+let write=confirm(" Do you want to write  ? ")      // return boolean value..
+if(write){
+    
+    console.log(a)
+
+}
+else{
+    alert("plz allow me to write..")
+}
