7a06947099929f7ae2ee7a8c7320c28efaf3a9c7
Abhijit Das <abhijitd721201@gmail.com>
2023-12-04 12:13:33 +0530

    handeling the promises

---
+++ 55_promises_then()_catch().js
@@ -0,0 +1,32 @@
+// resolve
+let p1 = new Promise((resolve,reject)=>{
+    console.log("your promises is pending...")
+    setTimeout(()=>{
+        console.log("i am a promises and i am fulfilled..")
+        resolve(true)
+    }, 5000)
+
+})
+
+
+// reject
+let p2 = new Promise((resolve,reject)=>{
+    console.log("your promises is pending...")
+    setTimeout(()=>{
+        console.log("i am a promises and i am rejected..")
+        reject(new Error("rejected for error in code .."))
+    }, 5000)
+
+})
+
+
+// To get the value..
+p1.then((value)=>{
+    console.log(value)
+})
+console.log(p1,p2)
+
+// To catch the error...
+p2.catch((error)=>{
+    console.log("some error occurred in p2..")
+})
