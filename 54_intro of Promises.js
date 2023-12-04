694c2ec98b2c0374d8269b4212be936244e90479
Abhijit Das <abhijitd721201@gmail.com>
2023-12-04 11:26:56 +0530

    create a new promises

---
+++ 54_intro of Promises.js
@@ -0,0 +1,19 @@
+// promises is used to run the code parallel exicution..
+
+let promises =new Promise(function(resolve,reject){
+    alert("hi promises")
+    resolve(56)
+})
+
+
+
+
+console.log("hello No 1")
+setTimeout(function(){
+    console.log("hello 2 in 4 second")
+},4000)
+
+console.log("My name is " + "Abhijit Das ")
+
+// call the promises 
+console.log(promises)
