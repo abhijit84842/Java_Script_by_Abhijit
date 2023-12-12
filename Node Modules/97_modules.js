d17b0c6f6becd849f2d43db23532885a84fd75d4
Abhijit Das <abhijitd721201@gmail.com>
2023-12-12 11:41:33 +0530

    create a module

---
+++ Node Modules/97_modules.js
@@ -0,0 +1,6 @@
+// to create npm project = npm init -y
+// common js modules
+
+
+
+console.log("hi i am abhi")
---
+++ Node Modules/modules1.js
@@ -0,0 +1,5 @@
+const hello =()=>{
+    console.log("hello Abhijit")
+}
+
+module.exports=hello;   // to use this mmodules outer package.
