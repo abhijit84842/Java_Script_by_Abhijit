168a17fcbd413ec29720137237713dec189b2587
Abhijit Das <abhijitd721201@gmail.com>
2023-12-14 17:47:40 +0530

    first commit

---
+++ 20_practice set of Array.js
@@ -0,0 +1,5 @@
+// q.s 1-> create an array of number and take input from the user to add numbers to this array..
+let arr=[1,2,3,4,5,6,7,8,9]
+let num=parseInt(prompt("enter a number : "))
+arr.push(num)
+console.log(arr)
