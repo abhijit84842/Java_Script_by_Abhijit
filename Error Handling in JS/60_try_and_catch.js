68b7b1844d71837a93a5d9852f80eb1324537970
Abhijit Das <abhijitd721201@gmail.com>
2023-12-14 18:16:24 +0530

    first commit

---
+++ Error Handling in JS/60_try_and_catch.js
@@ -0,0 +1,28 @@
+setTimeout(()=>{
+    
+    console.log("Hacking wifi ... please wait...")
+},1000)
+
+// handling the error...
+try{
+    console.log(rahul)
+}
+catch(err){
+    console.log("one error occured pls soleve it ....")
+}
+
+setTimeout(()=>{
+    
+    console.log("Fetching username and password....please wait...")
+},2000)
+
+setTimeout(()=>{
+    
+    console.log("Hacking Rahul's facekbook id ...please wait...")
+},4000)
+
+setTimeout(()=>{
+    
+    console.log("Rahul's facekbook id hacked...")
+},6000)
+
