e1e6dc48e244113ccedc1951d1af647145e40303
Abhijit Das <abhijitd721201@gmail.com>
2023-12-14 18:45:50 +0530

    first commit

---
+++ Error Handling in JS/62_Finally_Clause.js
@@ -0,0 +1,14 @@
+try{
+    let a = 0;
+    console.log(program)
+    console.log("Program run successfully...")
+}
+catch(error){
+    console.log("This is a error..")
+    console.log(p)      // error in catch
+}
+
+/*finally{
+    console.log("Finally execute always...")
+}
+*/ 
