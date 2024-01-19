7b0bb9070b176f744202251e2d074159d2b2f8ee
Abhijit Das <abhijitd721201@gmail.com>
2024-01-19 16:05:48 +0530

    updated

--- 51_Synchronous_Asynchronous.js
+++ 51_Synchronous_Asynchronous.js
@@ -1,9 +1,9 @@
 // Synchronous programming=> As the name suggests synchronous means to be in a sequence, i.e. every statement of the code gets executed one by one. So, basically a statement has to wait for the earlier statement to get executed.
-/*
+
 console.log("Hi")
 console.log("I am Abhijit Das")
 console.log("Its the javascript session")
-*/
+
 
 
 // Asynchronous Programming => it will not wait for earlier statement execution..it will executed after a specefic time.
