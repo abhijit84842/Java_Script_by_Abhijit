957234124e31d3c672bc12cbddfd4106bd3e0447
Abhijit Das <abhijitd721201@gmail.com>
2024-08-23 12:31:36 +0530

    real const object

--- Output Base Question/question 1.js
+++ Output Base Question/question 1.js
@@ -4,6 +4,23 @@
 const Tech = {
     name :"JS"
 }
+/*
+Tech ={
+    roll:24         // TypeError: Assignment to constant variable.
+} */
 Tech.name ="React"
 
 console.log(Tech.name)      // Output => React
+
+
+
+// 2) how we can make a real object to real const
+const Tech ={
+    name : "JS"
+}
+Tech.version ={name:1 }     // Create Nested object using version keyword
+Object.freeze(Tech)     // freeze the object So, it will not update
+Tech.name ="React"
+Tech.version.name="Aditya"  // we can update nested object 
+console.log(Tech.name)      // Output => JS (not updated)
+console.log(Tech.version.name) // Output => Aditya
