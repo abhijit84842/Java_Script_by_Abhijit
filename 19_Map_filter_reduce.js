3f306145f5dff7bf8837481973f52d1be5503257
Abhijit Das <abhijitd721201@gmail.com>
2024-12-11 15:30:24 +0530

    add forEach method

--- 19_Map_filter_reduce.js
+++ 19_Map_filter_reduce.js
@@ -25,3 +25,22 @@ let newValue=n.reduce((a,b)=>{
 })
 
 console.log(newValue)           // output -> 6
+
+
+// forEach()
+// The forEach() method in JavaScript is used to iterate over an array and execute a function for each element. It does not return a new array but is typically used for side effects, like logging or modifying something outside the array.
+// forEach() does not return a new array. If you need to transform an array, use map() instead.
+// forEach() does not support break or continue. If you need to stop iteration early, consider using a for loop or some().
+
+
+// Basic Example:-
+const fruits = ["apple", "banana", "cherry"];
+
+fruits.forEach(function(fruit) {
+  console.log(fruit);
+});
+
+// Output:
+// apple
+// banana
+// cherry
