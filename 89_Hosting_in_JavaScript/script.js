6ec224d491ef344bc9f1818fb4251246a5f39b17
Abhijit Das <abhijitd721201@gmail.com>
2024-12-12 12:50:04 +0530

    update

--- 89_Hosting_in_JavaScript/script.js
+++ 89_Hosting_in_JavaScript/script.js
@@ -6,7 +6,34 @@ In other words; a variable can be used before it has been declared
 
 /*
 Hosting => its refers to the process  where the interpreter appers to move the diclarations to the top of the code before execution.
+
+//  *** Declearation are hoisted to the top but not initialization...
+
+NOTE:- 1) javaScript only hoists the declearation not initialization
+
+// Function , class , var are hoisted(declearation) to the top but..
+// **** Function Expression , Class Expression , let const are not hoisted to the top. that means we can't access before its decleration.
+
+
 */
+
+// function => are hoisted..
+goodmorning()
+
+// function declearation :- it goes to the top before it execution
+function goodmorning(){
+    console.log("Good Morning")
+}
+
+
+// Function Expression = > are not Hoisted..
+great()
+let great= function(){
+    console.log("This is Great..")
+}
+
+
+
 x = 5; // Initialize 5 to x
 console.log(x)                  // Display x 
 var x; // Declare x
@@ -30,7 +57,7 @@ The variable is in a "temporal dead zone" from the start of the block until it i
 // for let 
 carName="BMW"       // Initialize 
 let carName;    // Declare 
-console.log(carName)
+console.log(carName)    // error
 
 
 /*
